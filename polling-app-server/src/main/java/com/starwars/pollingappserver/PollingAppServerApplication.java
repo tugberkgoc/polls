de036afd83c045638fb42d2212eb59dc6d450ef0
tugberkgoc <tugberk.goc@bilgiedu.net>
2018-09-30 17:06:53 +0300

    first commit

---
+++ polling-app-server/src/main/java/com/starwars/pollingappserver/PollingAppServerApplication.java
@@ -0,0 +1,12 @@
+package com.starwars.pollingappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingAppServerApplication.class, args);
+	}
+}
---
+++ polling-app-server/src/test/java/com/starwars/pollingappserver/PollingAppServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.starwars.pollingappserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PollingAppServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
